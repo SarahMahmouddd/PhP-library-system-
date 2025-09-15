0ddfe874398779a837bb384edeeedf81fcb6f0f6
SarahMahmouddd <Sarah.20367686@compit.aun.edu.eg>
2025-09-15 19:11:49 +0300

    Add files via upload

--- database/dbb.sql
+++ database/dbb.sql
@@ -1,1 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 15, 2025 at 05:20 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(50) NOT NULL,
+  `isbn` varchar(100) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `author`, `isbn`, `image`, `quantity`) VALUES
+(1, 'title1', 'author1', 'isbn1', '', 11),
+(2, 'title2', 'author2', 'isbn2', '', 10),
+(4, 'title3', 'author3', 'isbn3', '', 14),
+(7, 'title4', 'author4', 'isbn4', '', 13),
+(8, '', '', '', '', 0),
+(11, '', '', 'isbn5', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','user') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`id`, `name`, `email`, `password`, `role`) VALUES
+(1, 'yamin', 'yamin@email.com', 'yamin', 'admin'),
+(8, 'example', 'email@email.com', 'example', 'admin'),
+(9, 'example2', 'email1@email.com', 'example2', 'user'),
+(10, 'example3', 'email3@email.com', 'example3', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `issue_date` date NOT NULL,
+  `return_date` date DEFAULT NULL,
+  `status` enum('borrowed','returned') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `user_id`, `book_id`, `issue_date`, `return_date`, `status`) VALUES
+(6, 9, 2, '2025-09-15', '2525-09-10', 'borrowed'),
+(7, 9, 2, '2025-09-15', '2525-09-09', 'borrowed'),
+(9, 9, 2, '2025-09-15', NULL, 'borrowed'),
+(10, 9, 4, '2025-09-15', '2525-09-10', 'returned'),
+(11, 9, 2, '2025-09-15', '2525-09-10', 'returned'),
+(12, 9, 7, '2025-09-15', NULL, 'borrowed'),
+(13, 10, 1, '2025-09-15', NULL, 'borrowed'),
+(14, 10, 2, '2025-09-15', '2525-09-09', 'returned'),
+(15, 9, 1, '2025-09-15', NULL, 'borrowed');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `isbn` (`isbn`);
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
